3547e8c94ed06505642a66ba9613cdace1af0c41
B-S-Jang <83987659+mirgem2@users.noreply.github.com>
2021-06-06 15:35:32 +0900

    Update MainMenu.java

--- Team Project/SamePairs/src/MainMenu.java
+++ Team Project/SamePairs/src/MainMenu.java
@@ -15,6 +15,7 @@ public class MainMenu extends JFrame {
 	private String name;
 	private int userCnt = 0;
 	Person[] user = new Person[100];
+
 	
 	public MainMenu() {
 		super("MainMenu");
@@ -22,8 +23,8 @@ public class MainMenu extends JFrame {
 		menuJList = new JList<String>(menu);
 		menuJList.setVisibleRowCount(5);
 		menuJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		add(new JScrollPane(menuJList));
-		
+		add(new JScrollPane(menuJList));		
+
 		menuJList.addListSelectionListener(
 			new ListSelectionListener() {
 				@Override
@@ -31,39 +32,58 @@ public class MainMenu extends JFrame {
 					
 					if(!event.getValueIsAdjusting()) { // not to display twice
 						switch (menuJList.getSelectedIndex()) {
-							case 1:
-								name = JOptionPane.showInputDialog("Enter Name"); // fix : only when user choose ok
-								user[++userCnt] = new Person();
-								user[userCnt].setPerson(name);
-								DisplayButton level1 = new DisplayButton(20); // create ButtonFrame, fix : change to loop
-								level1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-								level1.setSize(1400, 800); // set frame size
-								level1.setVisible(true); // display frame
-								user[userCnt].setPersonScore(level1.getScore());
-								break;
+						case 0 : 
+							DisplayButton tutorial = new DisplayButton(4); // create ButtonFrame, fix : change to loop
+							tutorial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+							tutorial.setSize(1400, 800); // set frame size
+							tutorial.setVisible(true); // display frame
+							user[userCnt].setPersonScore(tutorial.getScore());
+							break;
+						case 1 :
+							name = JOptionPane.showInputDialog("Enter Name"); // fix : only when user choose ok
+							if(name == null)
+								return;
+							user[++userCnt] = new Person();
+							user[userCnt].setPerson(name);
+							for(int i = 1 ; i <= 3 ; i++) {
+								DisplayButton newGame = new DisplayButton((i + 1)*10); // create ButtonFrame, fix : change to loop
+								newGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+								newGame.setSize(1400, 800); // set frame size
+								newGame.setVisible(true); // display frame
+								user[userCnt].setPersonScore(newGame.getScore());
+							}
+							break;
 							
-							case 2:
-								String name2 = JOptionPane.showInputDialog("Enter Name");
-								int i = 0, j = 0, lastLevel = 0;
-								for (; i <= userCnt; i++) {
-									if (name2.equals(user[i].getPersonName())) {
-										j = 1;
-										lastLevel = user[i].getPersonHighLevel();
-									}
+						case 2 :
+							String name2 = JOptionPane.showInputDialog("Enter Name");
+							int i = 1, j = 0, lastLevel = 0, userNum = 0;
+							for (; i <= userCnt; i++) {
+								if (name2.equals(user[i].getPersonName())) {
+									j = 1; userNum = i;
+									lastLevel = user[i].getPersonHighLevel();
+									break;
 								}
-								/*if (j != 1) {
-									for (i = 0; i<)
-									DisplayButton loadedGame = new DisplayButton((lastLevel + 1)*10);
-									
-								}*/
+							}
+							if (j == 1) {
+								for (i = lastLevel; i <= 3; i++) {
+									DisplayButton loadedGame = new DisplayButton((i + 1)*10);
+									loadedGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+									loadedGame.setSize(1400, 800); // set frame size
+									loadedGame.setVisible(true); // display frame
+									user[userNum].setPersonScore(loadedGame.getScore());
+								}
+							}
+						case 3 :
+							
+						case 4 :
+							
+
 						}
 					}
 				}					
 			}
 
 		);
-	}
-	public String getName() {
-		return Name;
+
 	}
 }
