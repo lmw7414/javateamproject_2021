db0e021b1dda8ccbcb9ae2b3844c8e8804e7c46b
B-S-Jang <83987659+mirgem2@users.noreply.github.com>
2021-06-06 11:39:42 +0900

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,6 @@
+public enum Word {
+
+	apple, kiwi, banana, soup, strawberry, pizza, burger, chichen, lemon, tomato, cherry, mushroom, carrot, cake,
+	radish, onion, corn, broccoli, mango, cucumber
+
+}
