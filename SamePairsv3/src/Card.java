235528fd1fbfc05d6cbff306d4b464ea46183108
이민우 <lmw7414@naver.com>
2021-06-10 21:12:33 +0900

    add TTS module

---
+++ SamePairsv3/src/Card.java
@@ -0,0 +1,45 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.Icon;
+
+public class Card {
+   private String cardName;
+   private Status state;
+   private Icon picture;
+
+   
+   public Card(String cardName, Status state, Icon picture) {
+      this.cardName = cardName;
+      this.state = state;
+	  this.picture = picture;
+
+   }
+   
+   void setCardWord(String cardName) {
+      this.cardName = cardName;
+   }
+   void setCardState(Status state) {
+      this.state = state;
+   }
+   
+   void setCardIcon(Icon picture) {
+	   this.picture = picture;
+   }
+   public String getCardWord() {
+      return this.cardName;
+   }
+ 
+   public Status getCardState() {
+      return this.state;
+   }  
+   
+   public Icon getCardPicture() {
+	   return this.picture;
+   }
+  
+   @Override
+   public String toString()
+   {
+	   return this.cardName;
+   }
+}
