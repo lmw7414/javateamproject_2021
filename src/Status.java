36fd8c84bf2e4246f49e081db64b7f17b37209d1
이민우 <lmw7414@naver.com>
2021-06-12 21:15:41 +0900

    Delete src directory

--- src/Status.java
+++
@@ -1,3 +0,0 @@
-public enum Status {
-	    back, front, correct
-}
--- src/Word.java
+++
@@ -1,6 +0,0 @@
-public enum Word {
-
-	apple, kiwi, banana, soup, strawberry, pizza, burger, chichen, lemon, tomato, cherry, mushroom, carrot, cake,
-	radish, onion, corn, broccoli, mango, cucumber
-
-}
