c06c254030701ca64bf75b7e54c902edb26ed12e
이민우 <lmw7414@naver.com>
2021-06-12 21:14:52 +0900

    Delete SamePairsv3 directory

--- SamePairsv3/src/Word.java
+++
@@ -1,7 +0,0 @@
-
-public enum Word {
-
-	apple, kiwi, banana, soup, strawberry, pizza, burger, chicken, lemon, tomato, cherry, mushroom, carrot, cake,
-	radish, onion, corn, broccoli, mango, cucumber
-
-}
